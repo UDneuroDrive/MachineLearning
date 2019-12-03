3512c45e87553af8c7e459801a35876f41f94955
Justus Matteson <48027696+mato9020@users.noreply.github.com>
2019-12-02 21:09:30 -0500

    Add files via upload

---
+++ ml.py
@@ -0,0 +1,77 @@
+# USAGE
+# python train.py --dataset dataset --model fashion.model --labelbin mlb.pickle
+
+# set the matplotlib backend so figures can be saved in the background
+import matplotlib
+matplotlib.use("Agg")
+import tensorflow
+from tensorflow import keras
+# import the necessary packages
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import Adam
+from keras.preprocessing.image import img_to_array
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.model_selection import train_test_split
+from pyimagesearch.smallervggnet import SmallerVGGNet
+import matplotlib.pyplot as plt
+from imutils import paths
+import numpy as np
+import argparse
+import random
+import pickle
+import cv2
+
+import os
+import pandas as pn
+
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--dataset", required=True,
+	help="path to input dataset (i.e., directory of images)")
+ap.add_argument("-m", "--model", required=True,
+	help="path to output model")
+ap.add_argument("-l", "--labelbin", required=True,
+	help="path to output label binarizer")
+ap.add_argument("-p", "--plot", type=str, default="plot.png",
+	help="path to output accuracy/loss plot")
+args = vars(ap.parse_args())
+
+# initialize the number of epochs to train for, initial learning rate,
+# batch size, and image dimensions
+EPOCHS = 75
+INIT_LR = 1e-3
+BS = 32
+IMAGE_DIMS = (96, 96, 3)
+
+# grab the image paths and randomly shuffle them
+print("[INFO] loading images...")
+imagePaths = sorted(list(paths.list_images(args["dataset"])))
+random.seed(42)
+random.shuffle(imagePaths)
+
+# initialize the data and labels
+data = []
+labels = []# loop over the input images
+
+directionArr = pn.read_csv("data.csv")
+
+
+#spoof data images
+for imagePath in imagePaths:
+	# load the image, pre-process it, and store it in the data list
+	image = cv2.imread(imagePath)
+	image = cv2.resize(image, (IMAGE_DIMS[1], IMAGE_DIMS[0]))
+	image = img_to_array(image)
+	data.append(image)
+
+	# extract set of class labels from the image path and update the
+	# labels list
+	
+
+# scale the raw pixel intensities to the range [0, 1]
+data = np.array(data, dtype="float") / 255.0
+labels = np.array(labels)
+
+print("[INFO] data matrix: {} images ({:.2f}MB)".format(
+	len(imagePaths), data.nbytes / (1024 * 1000.0)))
